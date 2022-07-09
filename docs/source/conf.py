d01c923e54ada9676708f996f9f975d1e1207e2e
Aswin S <aswinsnair028@gmail.com>
2022-07-09 11:31:30 +0530

    fix docs, 0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser','sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autodoc', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
