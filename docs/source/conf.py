c6c96c32ceb9aa4c120a049f98bdbf762b37f96e
Aswin S <aswinsnair028@gmail.com>
2022-07-09 12:30:22 +0530

    fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['weheartpy', 'sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autodoc', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
